f4cc4572549d1bd8a3b06c997eb35f8a5f2d4908
FUKAI Kohei <K-Fukai@users.noreply.github.com>
2023-10-03 17:39:22 +0900

    talks

--- js/talks.js
+++ js/talks.js
@@ -1,47 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('data/publications.json')
+    fetch('data/talks.json')
         .then(response => response.json())
-        .then(publications => {
+        .then(talks => {
             const publishedContainer = document.getElementById('published-container');
             const preprintsContainer = document.getElementById('preprints-container');
             
-            publications.forEach(publication => {
+            talks.forEach(talk => {
                 const li = document.createElement('li');
                  // Create a link element for the title
                  const titleLink = document.createElement('a');
-                 titleLink.href = publication.link; // Assuming your JSON has a link property for each publication
-                 titleLink.textContent = publication.title;
+                 titleLink.href = talk.link; // Assuming your JSON has a link property for each talk
+                 titleLink.textContent = talk.title;
                  titleLink.target = "_blank"; // Optional: Opens the link in a new tab
  
                  const title = document.createElement('h2');
-                 title.className = 'publication-title';
+                 title.className = 'talk-title';
                  title.appendChild(titleLink); // Append the link to the title
                  li.appendChild(title); // Append the title to the list item
  
                  const authors = document.createElement('p');
-                 authors.className = 'publication-authors';
-                 authors.textContent = `${publication.authors}`;
+                 authors.className = 'talk-authors';
+                 authors.textContent = `${talk.authors}`;
                  li.appendChild(authors); // Append the authors to the list item
                  
                
                 
-                // Append the list item to the correct container based on publication type
-                if (publication.type === 'published') {
+                // Append the list item to the correct container based on talk type
+                if (talk.type === 'published') {
                     const citation = document.createElement('p');
-                    citation.className = 'publication-citation';
+                    citation.className = 'talk-citation';
                     
-                    citation.innerHTML = `${publication.journal}, <span class='bold'>${publication.volume}</span>, ${publication.page} (${publication.year}).`;
+                    citation.innerHTML = `${talk.journal}, <span class='bold'>${talk.volume}</span>, ${talk.page} (${talk.year}).`;
                     li.appendChild(citation);
 
                     const ol = publishedContainer.querySelector('ol') || document.createElement('ol');
                     if (!publishedContainer.contains(ol)) publishedContainer.appendChild(ol);
                     ol.appendChild(li);
-                } else if (publication.type === 'preprint') {
+                } else if (talk.type === 'preprint') {
 
                     const citation = document.createElement('p');
-                    citation.className = 'publication-citation';
+                    citation.className = 'talk-citation';
                    
-                    citation.innerHTML = `${publication.journal} (${publication.year}).`;
+                    citation.innerHTML = `${talk.journal} (${talk.year}).`;
                     li.appendChild(citation);
 
 
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
         })
-        .catch(error => console.error('Error fetching publication data:', error));
+        .catch(error => console.error('Error fetching talk data:', error));
 });
 
 
